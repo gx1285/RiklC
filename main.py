b1108f425499584a52661c717b325b951e66ead9
gx1285 <78240988+gx1285@users.noreply.github.com>
2023-01-07 21:04:56 +0900

    Update and rename git.py to main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import subprocess
 
+subprocess.call("npx rimraf RiklC".split())
 subprocess.call("git clone https://github.com/gx1285/RiklC.git".split())
 subprocess.call("python RiklC/bot.py".split())
